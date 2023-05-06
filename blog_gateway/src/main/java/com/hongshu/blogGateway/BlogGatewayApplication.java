755ba5bc065ff65195b24e680e76a1c71c91f23f
HongShu995 <759631477@qq.com>
2023-05-06 16:57:13 +0800

    UPDATE:first commit

---
+++ blog_gateway/src/main/java/com/hongshu/blogGateway/BlogGatewayApplication.java
@@ -0,0 +1,15 @@
+package com.hongshu.blogGateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Author: HongShu995
+ * Date: 2023/5/6
+ */
+@SpringBootApplication
+public class BlogGatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BlogGatewayApplication.class, args);
+    }
+}
