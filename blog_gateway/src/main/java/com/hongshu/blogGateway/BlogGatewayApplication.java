e83961c11491695dde082642760ac7db509118de
wangyibing <2212977363@qq.com>
2023-05-09 10:07:19 +0800

    模块环境搭建成功

--- blog_gateway/src/main/java/com/hongshu/blogGateway/BlogGatewayApplication.java
+++ blog_gateway/src/main/java/com/hongshu/blogGateway/BlogGatewayApplication.java
@@ -2,6 +2,7 @@ package com.hongshu.blogGateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 /**
  * Author: HongShu995
